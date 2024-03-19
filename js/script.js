80ce15621f45076f9f16189a3f7a3b5da372dccf
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-03-19 18:22:38 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Pablo Fiallos
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const street = document.getElementById("name").value
+  const address = parseInt(document.getElementById("address-entered").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "You live on " + street + " " + address + "."
+}
